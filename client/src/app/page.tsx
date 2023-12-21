94597901c0c03850a87d4c7f654673f1f86395c1
Michael Zhan <michaelzhan2001@gmail.com>
2023-12-21 14:58:01 -0600

    Update UI and favicon

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -30,18 +30,18 @@ export default function Home() {
 
   return (
     <>
-      <div className="h-screen w-screen flex flex-col justify-center items-center">
-        <div>
-          <p className="text-4xl">Tic Tac Toe</p>
+      <div className="w-screen flex flex-col items-center">
+        <div className='mt-32 mb-24'>
+          <p className="text-6xl">Tic Tac Toe</p>
         </div>
-        <div className="flex flex-wrap items-center justify-center text-center bg-gray-100">
-          <div className="w-96">
-            <button type="button" onClick={createNewGame}>Create new game</button>
+        <div className="flex flex-wrap items-center justify-center text-center bg-gray-100 py-20">
+          <div className="w-80">
+            <button type="button" onClick={createNewGame} className='bg-green-300 border border-black p-3'>Create new game</button>
           </div>
-          <div className="w-96">
+          <div className="w-80">
             <form className="flex flex-col items-center" onSubmit={joinGame}>
-              <input type='text' name="gameID" id='gameID' placeholder="Enter a game ID" className="w-1/2"></input>
-              <button type="submit">Join game</button>
+              <input type='text' name="gameID" id='gameID' placeholder="Enter a game ID" className="w-1/2 placeholder:text-center mb-3 p-3 text-lg border-black border"></input>
+              <button type="submit" className='bg-green-300 border border-black p-3'>Join game</button>
             </form>
           </div>
         </div>
